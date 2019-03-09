df57e6188d08a03f68c48f3320ef064cd9406213
jasson <jeirod@eltiempo.com>
2019-03-09 07:50:28 -0500

    [inicial] commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # worker
+proyecto usado para cargar datos de una base a otra
---
+++ config/default.config.php
@@ -0,0 +1,18 @@
+<?php
+## proccess variables
+define("NUMBER_ITEMS_MIGRATE",     "");
+## log variables
+define("GENERAL_LOG_PATH",     "");
+define("FAIL_ITEM_LOG_PATH",     "");
+## origin database variables
+define("ORIGIN_DB_HOST",     "");
+define("ORIGIN_DB_PORT",     "");
+define("ORIGIN_DB_USER",     "");
+define("ORIGIN_DB_PASS",     "");
+define("ORIGIN_DB_TABLE",     "");
+## DESTINY database variables
+define("DESTINY_DB_HOST",     "");
+define("DESTINY_DB_PORT",     "");
+define("DESTINY_DB_USER",     "");
+define("DESTINY_DB_PASS",     "");
+define("DESTINY_DB_TABLE",     "");
